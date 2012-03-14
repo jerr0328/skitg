3853c2499cbebc66a17ba8427a95eb810389ebe4
Jeremy <jerr0328@gmail.com>
2012-03-14 15:13:50 -0400

    Started project with libgdx engine (note the libs are too big to fit on Google Code)

---
+++ src/edu/ucf/cop4331/skitg/DesktopStarter.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package edu.ucf.cop4331.skitg;
+
+import com.badlogic.gdx.backends.jogl.JoglApplication;
+
+/**
+ * @author Jeremy Mayeres
+ * Starts the game for the desktop version
+ */
+public class DesktopStarter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new JoglApplication(new Skitg(), "Swift Kick in the Grass", 800, 480, false);
+
+	}
+
+}
---
+++ src/edu/ucf/cop4331/skitg/Skitg.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package edu.ucf.cop4331.skitg;
+
+import com.badlogic.gdx.ApplicationListener;
+
+/**
+ * @author Jeremy Mayeres
+ *
+ */
+public class Skitg implements ApplicationListener {
+
+	/**
+	 * The create method gets run on the first load
+	 */
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * The dispose method gets run when the game is quitting
+	 */
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Pause is called when the game gets suspended (i.e. phone call, etc.).
+	 * This also gets called before dispose() on quitting the game.
+	 */
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * This is essentially where the core code goes.
+	 * This gets called as often as possible
+	 */
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Resize is called any time the window size is changed. 
+	 * This can get called several times on loading.
+	 */
+	@Override
+	public void resize(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * This is what gets called when resuming the game after a pause().
+	 */
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
