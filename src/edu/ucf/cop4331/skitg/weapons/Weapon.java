57689601a128e81af159f07cfd8533c6708d79e0
Jeremy Mayeres <jerr0328@gmail.com>
2012-04-01 21:59:43 -0400

    Implementing Tank and Weapon skeleton

--- src/edu/ucf/cop4331/skitg/weapons/Weapon.java
+++ src/edu/ucf/cop4331/skitg/weapons/Weapon.java
@@ -1,5 +1,13 @@
 package edu.ucf.cop4331.skitg.weapons;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 public abstract class Weapon {
+	
+	
+	public abstract void update(float delta);
+	public abstract void render(SpriteBatch batch);
+	public abstract String toString();
+	
 
 }
