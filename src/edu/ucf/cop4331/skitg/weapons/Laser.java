ad36ac547c5058c98e516c3d51de3180ec252dad
Mishal Patel <Jagrat123@gmail.com>
2012-04-14 18:45:50 -0400

    Weapons

---
+++ src/edu/ucf/cop4331/skitg/weapons/Laser.java
@@ -0,0 +1,45 @@
+package edu.ucf.cop4331.skitg.weapons;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import edu.ucf.cop4331.skitg.Tank;
+
+public class Laser extends Weapon {
+
+	public Laser(Tank shooter) {
+		super(shooter);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(SpriteBatch batch) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void shoot() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Laser";
+	}
+
+	@Override
+	public TextureRegion getIcon() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
