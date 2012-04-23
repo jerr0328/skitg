51fb87d27dc8e8bcd99624163cbb345720e94b35
Jeremy Mayeres <jerr0328@gmail.com>
2012-04-22 21:40:38 -0400

    Cleaned up screen code

--- src/edu/ucf/cop4331/skitg/ui/GameScreen.java
+++ src/edu/ucf/cop4331/skitg/ui/GameScreen.java
@@ -5,26 +5,44 @@ import com.badlogic.gdx.Gdx;
 
 import edu.ucf.cop4331.skitg.Engine;
 
-
+/**
+ * Main game screen
+ * @author Jeremy Mayeres
+ *
+ */
 public class GameScreen extends SkitgScreen {
 	
+	// Game engine
 	Engine engine;
 
+	/**
+	 * Create the main game screen
+	 * @param game Game instance
+	 */
 	public GameScreen(Game game) {
 		super(game);
 	}
 	
+	/**
+	 * Show the screen
+	 */
 	public void show(){
 		engine = Engine.getInstance();
 		engine.initialize();
 	}
 	
+	/**
+	 * Render the screen
+	 */
 	public void render(float delta){
 		delta = Math.min(0.06f, Gdx.graphics.getDeltaTime());
 		engine.update(delta);
 		engine.render(delta);
 	}
 	
+	/**
+	 * Hide the screen
+	 */
 	public void hide(){
 		
 	}
--- src/edu/ucf/cop4331/skitg/ui/SkitgScreen.java
+++ src/edu/ucf/cop4331/skitg/ui/SkitgScreen.java
@@ -3,54 +3,71 @@ package edu.ucf.cop4331.skitg.ui;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
 
+/**
+ * Abstract class for all game screens
+ * @author Jeremy Mayeres
+ *
+ */
 public abstract class SkitgScreen implements Screen {
 	
+	// Game instance
 	Game game;
 	
+	/**
+	 * Construct the screen
+	 * @param game Game instance
+	 */
 	public SkitgScreen(Game game){
 		this.game = game;
 	}
 
+	/**
+	 * Dispose (close) the screen
+	 */
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-
 	}
 
+	/**
+	 * Hide the screen
+	 */
 	@Override
 	public void hide() {
-		// TODO Auto-generated method stub
-
 	}
 
+	/**
+	 * Pause the screen (i.e. phone call)
+	 */
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
-
 	}
 
+	/**
+	 * Render the screen
+	 */
 	@Override
 	public void render(float arg0) {
-		// TODO Auto-generated method stub
-
 	}
 
+	/**
+	 * Resize the screen
+	 */
 	@Override
 	public void resize(int arg0, int arg1) {
-		// TODO Auto-generated method stub
-
 	}
 
+	/**
+	 * Resume after a pause
+	 */
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
-
 	}
 
+	/**
+	 * Show the screen
+	 */
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
-
 	}
 
 }
